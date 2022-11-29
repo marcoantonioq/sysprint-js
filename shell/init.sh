fa5b5dcd257fca66f5a80202bbe26d3219f4d59b
Marco Antônio Queiroz <marco.aq7@gmail.com>
2022-11-29 09:09:13 -0300

    Remove update

--- shell/init.sh
+++ shell/init.sh
@@ -9,6 +9,21 @@ sleep 20
 lpadmin -p cups-pdf -v cups-pdf:/ -E -P /usr/share/ppd/cups-pdf/CUPS-PDF.ppd
 
 # Atualiza servidor
+UPSTREAM=${1:-'@{u}'}
+LOCAL=$(git rev-parse @)
+REMOTE=$(git rev-parse "$UPSTREAM")
+BASE=$(git merge-base @ "$UPSTREAM")
+
+if [ $LOCAL = $REMOTE ]; then
+    echo "Up-to-date"
+elif [ $LOCAL = $BASE ]; then
+    echo "Need to pull"
+elif [ $REMOTE = $BASE ]; then
+    echo "Need to push"
+else
+    echo "Diverged"
+fi
+
 git stash
 git pull --ff-only
 
