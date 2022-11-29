13905c48daed117ef553d57081df53f6c2f87c5f
Marco Antônio Queiroz <marco.aq7@gmail.com>
2022-11-29 09:16:38 -0300

    Script update

--- shell/init.sh
+++ shell/init.sh
@@ -9,27 +9,21 @@ sleep 20
 lpadmin -p cups-pdf -v cups-pdf:/ -E -P /usr/share/ppd/cups-pdf/CUPS-PDF.ppd
 
 # Atualiza servidor
-UPSTREAM=${1:-'@{u}'}
-LOCAL=$(git rev-parse @)
-REMOTE=$(git rev-parse "$UPSTREAM")
-BASE=$(git merge-base @ "$UPSTREAM")
+git fetch
+LOCAL=$(git rev-parse HEAD)
+REMOTE=$(git rev-parse @{u})
 
 if [ $LOCAL = $REMOTE ]; then
     echo "Up-to-date"
-elif [ $LOCAL = $BASE ]; then
-    echo "Need to pull"
-elif [ $REMOTE = $BASE ]; then
-    echo "Need to push"
 else
     echo "Diverged"
+    git stash
+    git pull --ff-only
+    # Atualiza app
+    npm install
+    # Build
+    npm run build
 fi
 
-git stash
-git pull --ff-only
-
-# Atualiza app
-npm install
-
 # Start
-npm run build
 npm run start
