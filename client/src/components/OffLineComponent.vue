10bb1670e04f52750880d820440c62409dbba2e0
Marco Antônio <marco.aq7@gmail.com>
2024-03-04 15:19:38 -0300

    Update

--- client/src/components/OffLineComponent.vue
+++ client/src/components/OffLineComponent.vue
@@ -1,7 +1,6 @@
 <template>
   <div class="offline-indicator q-pa-sm text-center" v-if="!runtime.connected">
     <q-icon name="wifi_off" color="red" size="150px" />
-    <p>Você está offline!</p>
     <p>Verifique sua conexão com a internet ou procure o suporte técnico.</p>
   </div>
 </template>
