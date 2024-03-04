48afbad55620e040c620ac090f248bd979b00697
Marco Antônio <marco.aq7@gmail.com>
2024-03-04 15:20:38 -0300

    update

--- client/src/components/OffLineComponent.vue
+++ client/src/components/OffLineComponent.vue
@@ -1,7 +1,10 @@
 <template>
   <div class="offline-indicator q-pa-sm text-center" v-if="!runtime.connected">
     <q-icon name="wifi_off" color="red" size="150px" />
-    <p>Verifique sua conexão com o servidor ou procure o suporte técnico.</p>
+    <p>
+      Verifique sua conexão com o servidor de impressão ou procure o suporte
+      técnico.
+    </p>
   </div>
 </template>
 
